b57f970ba3ab02039a7c40d230d2ef1748cc50fa
afiab <abidica30@stuy.edu>
2024-02-24 18:32:55 -0500

    added some arrow key movement

--- brick.py
+++ brick.py
@@ -17,6 +17,17 @@ while run:
 
     pygame.draw.rect(screen, (255,0, 0), player) #screen name, rgb, rectangle name
     
+    key = pygame.key.get_pressed() # know what key has been pressed
+    # arrow key movement
+    if key[pygame.K_a]: # move left
+        player.move_ip( -1, 0) #player moves in place -1 right (so left), and none up
+    elif key[pygame.K_d]: # move right
+        player.move_ip( 1, 0) #player moves in place 1 right and none up
+    elif key[pygame.K_w]: # move up
+        player.move_ip( 0, -1) #player moves in place none right and up 1
+    elif key[pygame.K_s]: # move up
+        player.move_ip( 0, 1) #player moves in place none right and up 1
+
     #event handler, looks for events like mouse clicks and keyboard presses
     for event in pygame.event.get(): #iterate through all events picked up by pygame
         #closing the game window
