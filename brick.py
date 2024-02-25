22ad2f921a8068411738c5419dc9865e215de0c2
afiab <abidica30@stuy.edu>
2024-02-24 19:41:03 -0500

    added constants for sprite width and height

--- brick.py
+++ brick.py
@@ -11,10 +11,12 @@ WHITE = (255,255,255) # pass in rgb vals as a tuple
 FPS = 60 # Frames per second
 
 # sprites: Ritchie and Bricky
+IMAGE_WIDTH, IMAGE_HEIGHT = 536/2.5,458/2.5
+
 RITCHIE_IMAGE = pygame.image.load(os.path.join('Assets', 'ritchie.png')) # ritchie
-RITCHIE_IMAGE = pygame.transform.scale(RITCHIE_IMAGE, (536/2.5,458/2.5)) #resize to size specified
+RITCHIE_IMAGE = pygame.transform.scale(RITCHIE_IMAGE, (IMAGE_WIDTH, IMAGE_HEIGHT)) #resize to size specified
 BRICKY_IMAGE = pygame.image.load(os.path.join('Assets', 'bricky.png')) # bricky
-BRICKY_IMAGE = pygame.transform.scale(BRICKY_IMAGE, (536/2.5,458/2.5)) #resize to size specified
+BRICKY_IMAGE = pygame.transform.scale(BRICKY_IMAGE, (IMAGE_WIDTH, IMAGE_HEIGHT)) #resize to size specified
 
 def draw_window():
     #updates the window
